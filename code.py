03f8700be2da20613f329d1dab7c50e493c99032
penda-diagne <59060758+penda-diagne@users.noreply.github.com>
2022-06-16 12:52:10 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,20 @@
-print("hello world")
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+import numpy as np
+import os
+import sys
+path_data_f0 = "hdfs://192.168.43.181:9870/home/datamemoire/F0"
+
+sys.path.append(path_data_f0)
+
+path2dataf0 = os.path.join(path_data_f0)
+
+full_filenames0 = os.listdir(path2dataf0)
+
+full_files0 = [os.path.join(path2dataf0,f) for f in full_filenames0]
+
+data=full_files0
+datadf=pd.DataFrame(columns=["id","path","label","niveau fibrose"])
+
+datadf.head()
