6304645703b22d3c40449d4e79f7305afd245f9d
penda-diagne <59060758+penda-diagne@users.noreply.github.com>
2022-06-16 13:39:13 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
+from pywebhdfs.webhdfs import PyWebHdfsClient
+from pprint import pprint
 
+hdfs = PyWebHdfsClient(host='192.168.43.181',port='9870')  # your Namenode IP & username here
+my_dir = '/home'
+pprint(hdfs.list_dir(my_dir))
